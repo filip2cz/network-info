5b480688178856cd0001f08f1da3f63ecac44627
filip2cz <filda.komarek@seznam.cz>
2023-06-06 17:44:27 +0200

    idk, somethink

--- network-info.Android/Resources/Resource.designer.cs
+++ network-info.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace network_info.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.0.0.73")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.0.99")]
 	public partial class Resource
 	{
 		
