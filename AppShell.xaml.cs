55ceb86e94f9bf21f572731260ba68a62659c56b
filip2cz <filip@fkomarek.eu>
2024-07-18 20:07:10 +0200

    MOVING TO MAUI

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace network_info
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/Tizen/Main.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using System;
+
+namespace network_info
+{
+    internal class Program : MauiApplication
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
